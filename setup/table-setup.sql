fc7f43ae3a9acba07565819af824e8fbd2a139d7
krishna-code-hub <krishna-code-hub>
2022-01-25 14:28:00 +0000

    push2

--- setup/table-setup.sql
+++ setup/table-setup.sql
@@ -38,3 +38,35 @@ insert into etl_fw.customers_dept_t1 values (1,'accounts',1000,'20220120');
 insert into etl_fw.customers_dept_t1 values (2,'maths',2000,'20220120');
 insert into etl_fw.customers_dept_t1 values (3,'housekeeping',3000,'20220120');
 insert into etl_fw.customers_dept_t1 values (4,'accounts',4000,'20220120');
+
+-- COMMAND ----------
+
+Create table etl_fw.customers_workdb
+(
+id INT,
+name string,
+country string,
+dept string,
+sal INT,
+ods string
+)
+using parquet 
+location "/mnt/rbdhub1000/retail-db/customers_workdb";
+
+-- COMMAND ----------
+
+Create table etl_fw.customers_t2
+(
+id INT,
+name string,
+country string,
+dept string,
+sal INT,
+start_dt string,
+end_dt string,
+process_name string,
+ods string
+)
+using delta
+partitioned by (ods)
+location "/mnt/rbdhub1000/retail-db/customers_t2";
